160bf03e0381a1d850aee19aca64b096089a725f
Angelo Junior Irakoze <s21391@pjwstk.edu.pl>
2025-11-14 02:19:36 +0100

    Modifying server.js and adding an api that fetch contacts.html

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Client")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "Client", "index.html"));
+});
+
+app.get("/api/get/contacts", (req, res) => {
+  res.sendFile(path.join(__dirname, "Client", "contacts.html"));
 });
 
 //Loading JSON profile to script.js
